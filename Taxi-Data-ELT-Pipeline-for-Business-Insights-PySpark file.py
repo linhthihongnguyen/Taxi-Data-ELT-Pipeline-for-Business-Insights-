71de8335266cdea6cfb049887fc71fbb6b0f83ef
Linh Nguyen <nguyenthihonglinh799@gmail.com>
2025-10-13 21:59:09 -0700

    Initial commit for Taxi-Data-ELT-Pipeline-for-Business-Insights

---
+++ Taxi-Data-ELT-Pipeline-for-Business-Insights-PySpark file.py
@@ -0,0 +1,290 @@
+# Databricks notebook source
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.stat import Correlation
+import pandas as pd
+
+# COMMAND ----------
+
+spark = SparkSession.builder.appName("ALY6110_EDA_Finalproject").getOrCreate()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # 1. Preprocessing
+
+# COMMAND ----------
+
+# a) Load dataset
+df = spark.read.csv("dbfs:/FileStore/tables/yellow_tripdata_2015_01.csv", header=True, inferSchema=True)
+df.show(5)
+
+# COMMAND ----------
+
+# b) Show schema
+df.printSchema()
+
+# COMMAND ----------
+
+# c) Count number of rows
+row_count = df.count()
+print(f"Number of rows: {row_count}")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # 1. Data Cleaning
+
+# COMMAND ----------
+
+# a) Check for nulls and missing values per column
+
+# Define numeric and non-numeric column lists
+numeric_cols = [field.name for field in df.schema.fields if str(field.dataType) in ('DoubleType', 'FloatType', 'IntegerType')]
+other_cols = [field.name for field in df.schema.fields if field.name not in numeric_cols]
+
+# Count nulls and NaNs for numeric columns
+numeric_nulls = df.select([
+    count(when(col(c).isNull() | isnan(col(c)), c)).alias(c) for c in numeric_cols
+])
+
+# Count only nulls for other (non-numeric) columns
+other_nulls = df.select([
+    count(when(col(c).isNull(), c)).alias(c) for c in other_cols
+])
+
+
+print("Missing values in numeric columns:")
+numeric_nulls.show()
+
+print("Missing values in non-numeric columns:")
+other_nulls.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC - There are no missing (null) or NaN values in the numeric columns
+# MAGIC - improvement_surcharge has 3 missing values. We can fill it with the most common to keep all data and avoid unnecessary row loss 
+
+# COMMAND ----------
+
+# Find the Most Frequent Value in improvement_surcharge
+improvement_surcharge_mode = df.groupBy("improvement_surcharge") .count().orderBy("count", ascending=False).first()['improvement_surcharge']
+print(improvement_surcharge_mode)
+
+# Ensure value is numeric (in case of implicit type mismatch)
+improvement_surcharge_mode = float(improvement_surcharge_mode)
+
+# Re-fill missing values again
+df = df.fillna({"improvement_surcharge": improvement_surcharge_mode})
+
+# Check again for missing values
+missing_check = df.select([
+    count(when(col(c).isNull(), c)).alias(c) for c in df.columns
+])
+print("Rechecking missing values:")
+missing_check.show()
+
+# COMMAND ----------
+
+# check duplicated rows
+# Group by all columns and count duplicates
+df.groupBy(df.columns).count().filter("count > 1").show()
+
+# COMMAND ----------
+
+# Remove duplicates
+df = df.dropDuplicates()
+
+# COMMAND ----------
+
+# Convert and Extract Time Features
+df = df \
+    .withColumn("pickup_hour", hour("tpep_pickup_datetime")) \
+    .withColumn("pickup_dayofweek", dayofweek("tpep_pickup_datetime")) \
+    .withColumn("pickup_month", month("tpep_pickup_datetime"))
+
+# COMMAND ----------
+
+ # Create a Trip Duration Column:
+ df = df.withColumn("trip_duration", 
+    (unix_timestamp("tpep_dropoff_datetime") - unix_timestamp("tpep_pickup_datetime")) / 60)
+
+# COMMAND ----------
+
+# Check Invalid Data
+
+invalid = df.filter(
+    (col("trip_distance") <= 0) |
+    (col("total_amount") <= 0) |
+    (col("fare_amount") <= 0)
+)
+
+invalid.show()
+
+# COMMAND ----------
+
+df = df.filter(
+    (col("trip_distance") > 0) &
+    (col("total_amount") > 0) &
+    (col("fare_amount") > 0)
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # 2. Exploratory Data Analysis (EDA)
+
+# COMMAND ----------
+
+# Descriptive Statistics
+df.describe(["trip_distance", "fare_amount", "tip_amount", "total_amount"]).show()
+
+# COMMAND ----------
+
+# Correlation Analysis
+# Select numeric columns for correlation
+num_cols = ['trip_distance', 'fare_amount', 'tip_amount', 'tolls_amount', 'trip_duration', 'total_amount']
+assembler = VectorAssembler(inputCols=num_cols, outputCol="features")
+df_vec = assembler.transform(df).select("features")
+
+# Compute correlation matrix
+corr_matrix = Correlation.corr(df_vec, "features").head()[0]
+print("Correlation Matrix:\n")
+print(corr_matrix)
+
+# COMMAND ----------
+
+# Correlation Analysis
+# Select numeric columns for correlation
+num_cols = ['trip_distance', 'fare_amount', 'tip_amount', 'tolls_amount', 'trip_duration', 'total_amount']
+
+assembler = VectorAssembler(inputCols=num_cols, outputCol="features")
+df_vector = assembler.transform(df).select("features")
+print(df_vector)
+# Compute correlation matrix
+correlation_matrix = Correlation.corr(df_vector, "features").head()[0].toArray()
+print(correlation_matrix)
+
+# COMMAND ----------
+
+# Create a DataFrame for easier formatting
+corr_df = pd.DataFrame(correlation_matrix, index=num_cols, columns=num_cols)
+corr_df.reset_index(inplace=True)
+corr_df = corr_df.melt(id_vars=["index"], var_name="feature", value_name="correlation")
+corr_df.columns = ["feature_1", "feature_2", "correlation"]
+
+# Convert to Spark DataFrame
+correlation_spark_df = spark.createDataFrame(corr_df)
+correlation_spark_df.show()
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC - tip_amount vs total_amount: 0.99995. This is nearly a perfect correlation — expected, since tips are included in the total fare. Tipping behavior contributes significantly to total cost.
+# MAGIC - fare_amount vs tolls_amount: 0.495 Moderate correlation, possibly due to tolls being common on longer or outer borough trips. Tolls tend to increase with base fare, often in fixed routes (e.g., airport trips).
+# MAGIC - trip_distance has no significant correlation with any field — surprising, but likely due to inconsistent pricing.
+# MAGIC - trip_duration has minimal correlation with all variables, suggesting duration isn’t a key factor in cost calculation for short city rides.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 1. Temporal Patterns – When is Taxi Demand Highest?
+
+# COMMAND ----------
+
+# Trip Volume by Hour
+df.groupBy("pickup_hour").count().orderBy("pickup_hour").show()
+
+# COMMAND ----------
+
+# Trip Volume by Day of Week:
+df.groupBy("pickup_dayofweek").count().orderBy("pickup_dayofweek").show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 2. Spatial Patterns – Which Zones Have Frequent Pickups/Drop-offs?
+
+# COMMAND ----------
+
+# Round Coordinates for Approximate Clustering:
+df = df.withColumn("pickup_lat_round", round("pickup_latitude", 2)) \
+       .withColumn("pickup_lon_round", round("pickup_longitude", 2)) \
+       .withColumn("dropoff_lat_round", round("dropoff_latitude", 2)) \
+       .withColumn("dropoff_lon_round", round("dropoff_longitude", 2))
+
+# Frequent Pickup Coordinates:
+df.groupBy("pickup_lat_round", "pickup_lon_round") \
+  .count().orderBy("count", ascending=False).show(10)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 3. Trip Duration & Congestion Patterns
+
+# COMMAND ----------
+
+# Avg Duration by Hour:
+df.groupBy("pickup_hour").avg("trip_duration").orderBy("pickup_hour").show()
+
+# Duration vs Distance
+df.select("trip_distance", "trip_duration").describe().show()
+
+# COMMAND ----------
+
+#  Filter High Duration on Low Distance 
+df.filter((col("trip_distance") < 1) & (col("trip_duration") > 15)).show(5)
+
+
+# COMMAND ----------
+
+# Taxi Trip Revenue and Tips Breakdown by Payment Type
+df_with_payment_label = df.withColumn(
+    "payment_label",
+    when(col("payment_type") == 1, "Credit card")
+    .when(col("payment_type") == 2, "Cash")
+    .when(col("payment_type") == 3, "No charge")
+    .when(col("payment_type") == 4, "Dispute")
+    .when(col("payment_type") == 5, "Unknown")
+    .when(col("payment_type") == 6, "Voided trip")
+    .otherwise("Other")
+)
+
+df_with_payment_label.groupBy("payment_label") \
+    .agg(avg("tip_amount").alias("avg_tip"), 
+         avg("total_amount").alias("avg_total"),
+         avg("fare_amount").alias("avg_fare")) \
+    .orderBy("payment_label") \
+    .show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # 3. Dashboard
+
+# COMMAND ----------
+
+spark.conf.set("spark.databricks.delta.schema.autoMerge.enabled", "true")
+# Save DataFrame as a Table
+df.write.format("delta").mode("overwrite").saveAsTable("taxi_data")
+
+# COMMAND ----------
+
+#  Save Correlation Table as SQL View
+correlation_spark_df.write.mode("overwrite").saveAsTable("correlation_matrix_view")
+
+# COMMAND ----------
+
+df.show(5)
+
+# COMMAND ----------
+
+df.printSchema()
---
+++ Taxi-Data-ELT-Pipeline-for-Business-Insights-SQL.sql
@@ -0,0 +1,581 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC ### 1. Create filters
+
+-- COMMAND ----------
+
+CREATE WIDGET DROPDOWN vendor_id DEFAULT '1' CHOICES SELECT DISTINCT VendorID FROM taxi_data ORDER BY VendorID;
+CREATE WIDGET DROPDOWN pickup_hour DEFAULT '0' CHOICES SELECT DISTINCT pickup_hour FROM taxi_data ORDER BY pickup_hour;
+CREATE WIDGET DROPDOWN passenger_count DEFAULT '1' CHOICES SELECT DISTINCT passenger_count FROM taxi_data ORDER BY passenger_count;
+CREATE WIDGET DROPDOWN store_and_fwd_flag DEFAULT 'N' CHOICES SELECT DISTINCT store_and_fwd_flag FROM taxi_data ORDER BY store_and_fwd_flag;
+
+
+-- COMMAND ----------
+
+CREATE WIDGET DROPDOWN payment_type DEFAULT 'Credit card'
+CHOICES
+SELECT DISTINCT
+  CASE payment_type
+    WHEN 1 THEN 'Credit card'
+    WHEN 2 THEN 'Cash'
+    WHEN 3 THEN 'No charge'
+    WHEN 4 THEN 'Dispute'
+    WHEN 5 THEN 'Unknown'
+    WHEN 6 THEN 'Voided trip'
+  END AS payment_label
+FROM taxi_data
+ORDER BY payment_label;
+
+-- COMMAND ----------
+
+CREATE WIDGET DROPDOWN pickup_dayofweek DEFAULT 'Mon'
+CHOICES
+SELECT DISTINCT
+  CASE pickup_dayofweek
+    WHEN 1 THEN 'Sun'
+    WHEN 2 THEN 'Mon'
+    WHEN 3 THEN 'Tue'
+    WHEN 4 THEN 'Wed'
+    WHEN 5 THEN 'Thu'
+    WHEN 6 THEN 'Fri'
+    WHEN 7 THEN 'Sat'
+  END AS day_name
+FROM taxi_data
+ORDER BY day_name;
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 2. Counters
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Number of Trips
+-- Total Number of Trips
+SELECT COUNT(*) AS total_trips
+FROM taxi_data
+WHERE CAST(VendorID AS STRING) = '${vendor_id}'
+  AND CASE payment_type
+        WHEN 1 THEN 'Credit card'
+        WHEN 2 THEN 'Cash'
+        WHEN 3 THEN 'No charge'
+        WHEN 4 THEN 'Dispute'
+        WHEN 5 THEN 'Unknown'
+        WHEN 6 THEN 'Voided trip'
+      END = '${payment_type}'
+  AND CAST(passenger_count AS STRING) = '${passenger_count}'
+  AND CAST(pickup_hour AS STRING) = '${pickup_hour}'
+  AND CASE pickup_dayofweek
+        WHEN 1 THEN 'Sun'
+        WHEN 2 THEN 'Mon'
+        WHEN 3 THEN 'Tue'
+        WHEN 4 THEN 'Wed'
+        WHEN 5 THEN 'Thu'
+        WHEN 6 THEN 'Fri'
+        WHEN 7 THEN 'Sat'
+      END = '${pickup_dayofweek}'
+  AND store_and_fwd_flag = '${store_and_fwd_flag}';
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Average Trip Distance
+-- Average Trip Distance
+SELECT ROUND(AVG(trip_distance), 2) AS avg_trip_distance FROM taxi_data
+WHERE CAST(VendorID AS STRING) = '${vendor_id}'
+  AND CASE payment_type
+        WHEN 1 THEN 'Credit card'
+        WHEN 2 THEN 'Cash'
+        WHEN 3 THEN 'No charge'
+        WHEN 4 THEN 'Dispute'
+        WHEN 5 THEN 'Unknown'
+        WHEN 6 THEN 'Voided trip'
+      END = '${payment_type}'
+  AND CAST(passenger_count AS STRING) = '${passenger_count}'
+  AND CAST(pickup_hour AS STRING) = '${pickup_hour}'
+  AND CASE pickup_dayofweek
+        WHEN 1 THEN 'Sun'
+        WHEN 2 THEN 'Mon'
+        WHEN 3 THEN 'Tue'
+        WHEN 4 THEN 'Wed'
+        WHEN 5 THEN 'Thu'
+        WHEN 6 THEN 'Fri'
+        WHEN 7 THEN 'Sat'
+      END = '${pickup_dayofweek}'
+  AND store_and_fwd_flag = '${store_and_fwd_flag}';
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Average Fare
+-- Average Fare Amount
+SELECT ROUND(AVG(fare_amount), 2) AS avg_fare FROM taxi_data
+WHERE CAST(VendorID AS STRING) = '${vendor_id}'
+  AND CASE payment_type
+        WHEN 1 THEN 'Credit card'
+        WHEN 2 THEN 'Cash'
+        WHEN 3 THEN 'No charge'
+        WHEN 4 THEN 'Dispute'
+        WHEN 5 THEN 'Unknown'
+        WHEN 6 THEN 'Voided trip'
+      END = '${payment_type}'
+  AND CAST(passenger_count AS STRING) = '${passenger_count}'
+  AND CAST(pickup_hour AS STRING) = '${pickup_hour}'
+  AND CASE pickup_dayofweek
+        WHEN 1 THEN 'Sun'
+        WHEN 2 THEN 'Mon'
+        WHEN 3 THEN 'Tue'
+        WHEN 4 THEN 'Wed'
+        WHEN 5 THEN 'Thu'
+        WHEN 6 THEN 'Fri'
+        WHEN 7 THEN 'Sat'
+      END = '${pickup_dayofweek}'
+  AND store_and_fwd_flag = '${store_and_fwd_flag}';
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Total Revenue
+-- Total Revenue 
+SELECT ROUND(SUM(total_amount), 2) AS total_revenue
+FROM taxi_data
+WHERE CAST(VendorID AS STRING) = '${vendor_id}'
+  AND CASE payment_type
+        WHEN 1 THEN 'Credit card'
+        WHEN 2 THEN 'Cash'
+        WHEN 3 THEN 'No charge'
+        WHEN 4 THEN 'Dispute'
+        WHEN 5 THEN 'Unknown'
+        WHEN 6 THEN 'Voided trip'
+      END = '${payment_type}'
+  AND CAST(passenger_count AS STRING) = '${passenger_count}'
+  AND CAST(pickup_hour AS STRING) = '${pickup_hour}'
+  AND CASE pickup_dayofweek
+        WHEN 1 THEN 'Sun'
+        WHEN 2 THEN 'Mon'
+        WHEN 3 THEN 'Tue'
+        WHEN 4 THEN 'Wed'
+        WHEN 5 THEN 'Thu'
+        WHEN 6 THEN 'Fri'
+        WHEN 7 THEN 'Sat'
+      END = '${pickup_dayofweek}'
+  AND store_and_fwd_flag = '${store_and_fwd_flag}';
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 3. Temporal Patterns – Taxi Demand by Time
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Taxi Demand by Hour
+-- Hourly Demand
+SELECT 
+  pickup_hour,
+  COUNT(*) AS total_trips
+FROM taxi_data
+WHERE CAST(VendorID AS STRING) = '${vendor_id}'
+  AND CASE payment_type
+        WHEN 1 THEN 'Credit card'
+        WHEN 2 THEN 'Cash'
+        WHEN 3 THEN 'No charge'
+        WHEN 4 THEN 'Dispute'
+        WHEN 5 THEN 'Unknown'
+        WHEN 6 THEN 'Voided trip'
+      END = '${payment_type}'
+  AND CAST(passenger_count AS STRING) = '${passenger_count}'
+  -- AND CAST(pickup_hour AS STRING) = '${pickup_hour}'
+  AND CASE pickup_dayofweek
+        WHEN 1 THEN 'Sun'
+        WHEN 2 THEN 'Mon'
+        WHEN 3 THEN 'Tue'
+        WHEN 4 THEN 'Wed'
+        WHEN 5 THEN 'Thu'
+        WHEN 6 THEN 'Fri'
+        WHEN 7 THEN 'Sat'
+      END = '${pickup_dayofweek}'
+AND store_and_fwd_flag = '${store_and_fwd_flag}'
+GROUP BY pickup_hour
+ORDER BY pickup_hour
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC The analysis of hourly taxi demand shows that trip volume is lowest between 2 AM and 6 AM, then steadily increases through the morning commute, peaking between 6 PM and 9 PM, with the highest activity at 7 PM (over 800,000 rides).
+-- MAGIC
+-- MAGIC These patterns clearly identify optimal windows for:
+-- MAGIC
+-- MAGIC - Fleet Deployment: Increase driver availability during evening peaks to meet demand and reduce passenger wait times.
+-- MAGIC
+-- MAGIC - Surge Pricing Models: Apply dynamic pricing during peak hours (especially post-work hours and nightlife) to balance supply and demand.
+-- MAGIC
+-- MAGIC - Predictive Dispatching: Use historical patterns to forecast ride demand and position vehicles preemptively in high-demand zones during peak times.
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Taxi demand by day of week
+-- Daily Demand
+SELECT 
+  pickup_dayofweek,
+  CASE pickup_dayofweek
+    WHEN 1 THEN 'Sunday'
+    WHEN 2 THEN 'Monday'
+    WHEN 3 THEN 'Tuesday'
+    WHEN 4 THEN 'Wednesday'
+    WHEN 5 THEN 'Thursday'
+    WHEN 6 THEN 'Friday'
+    WHEN 7 THEN 'Saturday'
+  END AS day,
+  COUNT(*) AS daily_trips
+FROM taxi_data
+WHERE CAST(VendorID AS STRING) = '${vendor_id}'
+  AND CASE payment_type
+        WHEN 1 THEN 'Credit card'
+        WHEN 2 THEN 'Cash'
+        WHEN 3 THEN 'No charge'
+        WHEN 4 THEN 'Dispute'
+        WHEN 5 THEN 'Unknown'
+        WHEN 6 THEN 'Voided trip'
+      END = '${payment_type}'
+  AND CAST(passenger_count AS STRING) = '${passenger_count}'
+  AND CAST(pickup_hour AS STRING) = '${pickup_hour}'
+  -- AND CASE pickup_dayofweek
+  --       WHEN 1 THEN 'Sun'
+  --       WHEN 2 THEN 'Mon'
+  --       WHEN 3 THEN 'Tue'
+  --       WHEN 4 THEN 'Wed'
+  --       WHEN 5 THEN 'Thu'
+  --       WHEN 6 THEN 'Fri'
+  --       WHEN 7 THEN 'Sat'
+  --     END = '${pickup_dayofweek}'
+  AND store_and_fwd_flag = '${store_and_fwd_flag}'
+GROUP BY pickup_dayofweek
+ORDER BY pickup_dayofweek
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC Taxi demand is highest on weekends, especially Saturdays, with over 2.3 million trips, and lowest on Mondays. This suggests that weekend surge pricing and increased fleet deployment would be highly effective. Weekdays like Monday and Tuesday may benefit from promotional pricing or reduced fleet to optimize costs.
+-- MAGIC
+-- MAGIC
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ###  4. Spatial Patterns – Frequent Pickup/Drop-off Zones
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Frequent Pickup/Drop-off Zones
+SELECT 
+  ROUND(pickup_latitude, 2) AS lat, 
+  ROUND(pickup_longitude, 2) AS lon,
+  COUNT(*) AS pickup_count
+FROM taxi_data
+WHERE CAST(VendorID AS STRING) = '${vendor_id}'
+  AND CASE payment_type
+        WHEN 1 THEN 'Credit card'
+        WHEN 2 THEN 'Cash'
+        WHEN 3 THEN 'No charge'
+        WHEN 4 THEN 'Dispute'
+        WHEN 5 THEN 'Unknown'
+        WHEN 6 THEN 'Voided trip'
+      END = '${payment_type}'
+  AND CAST(passenger_count AS STRING) = '${passenger_count}'
+  AND CAST(pickup_hour AS STRING) = '${pickup_hour}'
+  AND CASE pickup_dayofweek
+        WHEN 1 THEN 'Sun'
+        WHEN 2 THEN 'Mon'
+        WHEN 3 THEN 'Tue'
+        WHEN 4 THEN 'Wed'
+        WHEN 5 THEN 'Thu'
+        WHEN 6 THEN 'Fri'
+        WHEN 7 THEN 'Sat'
+      END = '${pickup_dayofweek}'
+  AND store_and_fwd_flag = '${store_and_fwd_flag}'
+GROUP BY lat, lon
+ORDER BY pickup_count DESC
+LIMIT 20
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 5. Trip Duration & Congestion by Time/Location
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Average trip duration by hour
+SELECT 
+  pickup_hour,
+  AVG(trip_duration) AS avg_duration
+FROM taxi_data
+WHERE CAST(VendorID AS STRING) = '${vendor_id}'
+  AND CASE payment_type
+        WHEN 1 THEN 'Credit card'
+        WHEN 2 THEN 'Cash'
+        WHEN 3 THEN 'No charge'
+        WHEN 4 THEN 'Dispute'
+        WHEN 5 THEN 'Unknown'
+        WHEN 6 THEN 'Voided trip'
+      END = '${payment_type}'
+  AND CAST(passenger_count AS STRING) = '${passenger_count}'
+  -- AND CAST(pickup_hour AS STRING) = '${pickup_hour}'
+  AND CASE pickup_dayofweek
+        WHEN 1 THEN 'Sun'
+        WHEN 2 THEN 'Mon'
+        WHEN 3 THEN 'Tue'
+        WHEN 4 THEN 'Wed'
+        WHEN 5 THEN 'Thu'
+        WHEN 6 THEN 'Fri'
+        WHEN 7 THEN 'Sat'
+      END = '${pickup_dayofweek}'
+  AND store_and_fwd_flag = '${store_and_fwd_flag}'
+GROUP BY pickup_hour
+ORDER BY pickup_hour
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Trip Duration by Hour
+--Boxplot Data: Trip Duration by Hour for Short Trips
+SELECT
+  pickup_hour,
+  trip_duration
+FROM taxi_data
+WHERE CAST(VendorID AS STRING) = '${vendor_id}'
+  AND CASE payment_type
+        WHEN 1 THEN 'Credit card'
+        WHEN 2 THEN 'Cash'
+        WHEN 3 THEN 'No charge'
+        WHEN 4 THEN 'Dispute'
+        WHEN 5 THEN 'Unknown'
+        WHEN 6 THEN 'Voided trip'
+      END = '${payment_type}'
+  AND CAST(passenger_count AS STRING) = '${passenger_count}'
+  -- AND CAST(pickup_hour AS STRING) = '${pickup_hour}'
+  AND CASE pickup_dayofweek
+        WHEN 1 THEN 'Sun'
+        WHEN 2 THEN 'Mon'
+        WHEN 3 THEN 'Tue'
+        WHEN 4 THEN 'Wed'
+        WHEN 5 THEN 'Thu'
+        WHEN 6 THEN 'Fri'
+        WHEN 7 THEN 'Sat'
+      END = '${pickup_dayofweek}'
+  AND store_and_fwd_flag = '${store_and_fwd_flag}';
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Congested Pickup Areas for Short Trips with Long Duration
+-- Congested Pickup Areas for Short Trips with Long Duration
+SELECT
+  ROUND(pickup_latitude, 2) AS lat_bin,
+  ROUND(pickup_longitude, 2) AS lon_bin,
+  COUNT(*) AS trip_count
+FROM taxi_data
+WHERE trip_distance < 1 AND trip_duration > 20
+  AND CAST(VendorID AS STRING) = '${vendor_id}'
+  AND CASE payment_type
+        WHEN 1 THEN 'Credit card'
+        WHEN 2 THEN 'Cash'
+        WHEN 3 THEN 'No charge'
+        WHEN 4 THEN 'Dispute'
+        WHEN 5 THEN 'Unknown'
+        WHEN 6 THEN 'Voided trip'
+      END = '${payment_type}'
+  AND CAST(passenger_count AS STRING) = '${passenger_count}'
+  AND CAST(pickup_hour AS STRING) = '${pickup_hour}'
+  AND CASE pickup_dayofweek
+        WHEN 1 THEN 'Sun'
+        WHEN 2 THEN 'Mon'
+        WHEN 3 THEN 'Tue'
+        WHEN 4 THEN 'Wed'
+        WHEN 5 THEN 'Thu'
+        WHEN 6 THEN 'Fri'
+        WHEN 7 THEN 'Sat'
+      END = '${pickup_dayofweek}'
+  AND store_and_fwd_flag = '${store_and_fwd_flag}'
+GROUP BY lat_bin, lon_bin
+ORDER BY trip_count DESC
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 6. Correlation analysis
+
+-- COMMAND ----------
+
+-- Correlation heatmap for the full data 
+SELECT * FROM correlation_matrix_view
+ORDER BY correlation DESC;
+
+-- COMMAND ----------
+
+-- Corelation analysis for filtered data
+-- Compute the correlation matrix with filters applied
+WITH filtered_data AS (
+    SELECT 
+        trip_distance, 
+        fare_amount, 
+        tip_amount, 
+        tolls_amount, 
+        trip_duration, 
+        total_amount
+    FROM taxi_data
+    WHERE CAST(VendorID AS STRING) = '${vendor_id}'
+      -- AND CASE payment_type
+      --       WHEN 1 THEN 'Credit card'
+      --       WHEN 2 THEN 'Cash'
+      --       WHEN 3 THEN 'No charge'
+      --       WHEN 4 THEN 'Dispute'
+      --       WHEN 5 THEN 'Unknown'
+      --       WHEN 6 THEN 'Voided trip'
+      --     END = '${payment_type}'
+      AND CAST(passenger_count AS STRING) = '${passenger_count}'
+      AND CAST(pickup_hour AS STRING) = '${pickup_hour}'
+      AND CASE pickup_dayofweek
+            WHEN 1 THEN 'Sun'
+            WHEN 2 THEN 'Mon'
+            WHEN 3 THEN 'Tue'
+            WHEN 4 THEN 'Wed'
+            WHEN 5 THEN 'Thu'
+            WHEN 6 THEN 'Fri'
+            WHEN 7 THEN 'Sat'
+          END = '${pickup_dayofweek}'
+      AND store_and_fwd_flag = '${store_and_fwd_flag}'
+)
+SELECT 
+    corr(trip_distance, fare_amount) AS corr_trip_distance_fare_amount, 
+    corr(trip_distance, tip_amount) AS corr_trip_distance_tip_amount,
+    corr(trip_distance, tolls_amount) AS corr_trip_distance_tolls_amount,
+    corr(fare_amount, tip_amount) AS corr_fare_amount_tip_amount,
+    corr(fare_amount, tolls_amount) AS corr_fare_amount_tolls_amount,
+    corr(tip_amount, tolls_amount) AS corr_tip_amount_tolls_amount,
+    corr(trip_duration, total_amount) AS corr_trip_duration_total_amount
+FROM filtered_data;
+
+
+
+
+-- COMMAND ----------
+
+CREATE OR REPLACE TEMP VIEW correlation_results AS
+WITH filtered_data AS (
+    SELECT 
+        trip_distance, 
+        fare_amount, 
+        tip_amount, 
+        tolls_amount, 
+        trip_duration, 
+        total_amount
+    FROM taxi_data
+    WHERE CAST(VendorID AS STRING) = '${vendor_id}'
+      AND CAST(passenger_count AS STRING) = '${passenger_count}'
+      AND CAST(pickup_hour AS STRING) = '${pickup_hour}'
+      AND CASE pickup_dayofweek
+            WHEN 1 THEN 'Sun'
+            WHEN 2 THEN 'Mon'
+            WHEN 3 THEN 'Tue'
+            WHEN 4 THEN 'Wed'
+            WHEN 5 THEN 'Thu'
+            WHEN 6 THEN 'Fri'
+            WHEN 7 THEN 'Sat'
+          END = '${pickup_dayofweek}'
+      AND store_and_fwd_flag = '${store_and_fwd_flag}'
+)
+SELECT 
+    corr(trip_distance, fare_amount) AS corr_trip_distance_fare_amount, 
+    corr(trip_distance, tip_amount) AS corr_trip_distance_tip_amount,
+    corr(trip_distance, tolls_amount) AS corr_trip_distance_tolls_amount,
+    corr(fare_amount, tip_amount) AS corr_fare_amount_tip_amount,
+    corr(fare_amount, tolls_amount) AS corr_fare_amount_tolls_amount,
+    corr(tip_amount, tolls_amount) AS corr_tip_amount_tolls_amount,
+    corr(trip_duration, total_amount) AS corr_trip_duration_total_amount
+FROM filtered_data;
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC import pandas as pd
+-- MAGIC import seaborn as sns
+-- MAGIC import matplotlib.pyplot as plt
+-- MAGIC
+-- MAGIC # Query the correlation results from the temporary view
+-- MAGIC correlation_query = """
+-- MAGIC SELECT * FROM correlation_results
+-- MAGIC """
+-- MAGIC
+-- MAGIC # Execute the SQL query and convert the result to a Pandas DataFrame
+-- MAGIC correlation_result = spark.sql(correlation_query).toPandas()
+-- MAGIC
+-- MAGIC # Reshape the correlation result into a matrix format (for heatmap)
+-- MAGIC # Since the result is a single row, we manually create a 2D array
+-- MAGIC correlation_matrix = correlation_result.values.reshape(1, -1)
+-- MAGIC
+-- MAGIC # Create a DataFrame for visualization purposes
+-- MAGIC correlation_df = pd.DataFrame(correlation_matrix, columns=correlation_result.columns)
+-- MAGIC
+-- MAGIC # Plot the heatmap using Seaborn
+-- MAGIC plt.figure(figsize=(10, 7))
+-- MAGIC sns.heatmap(correlation_df, annot=True, cmap='coolwarm', vmin=-1, vmax=1, fmt='.2f', linewidths=0.5)
+-- MAGIC
+-- MAGIC # Add a title
+-- MAGIC plt.title('Filterred Correlation Matrix Heatmap', fontsize=16)
+-- MAGIC plt.xticks(rotation=40, ha="right")
+-- MAGIC # Display the plot
+-- MAGIC plt.show()
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 7. Taxi Trip Revenue and Tips Breakdown by Payment Type
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Taxi Trip Revenue and Tips Breakdown by Payment Type
+SELECT
+  CASE payment_type
+    WHEN 1 THEN 'Credit card'
+    WHEN 2 THEN 'Cash'
+    WHEN 3 THEN 'No charge'
+    WHEN 4 THEN 'Dispute'
+    WHEN 5 THEN 'Unknown'
+    WHEN 6 THEN 'Voided trip'
+    ELSE 'Other'
+  END AS payment_label,
+  AVG(tip_amount) AS avg_tip,
+  AVG(total_amount) AS avg_total,
+  AVG(fare_amount) AS avg_fare
+FROM taxi_data
+WHERE CAST(VendorID AS STRING) = '${vendor_id}'
+  -- AND CASE payment_type
+  --       WHEN 1 THEN 'Credit card'
+  --       WHEN 2 THEN 'Cash'
+  --       WHEN 3 THEN 'No charge'
+  --       WHEN 4 THEN 'Dispute'
+  --       WHEN 5 THEN 'Unknown'
+  --       WHEN 6 THEN 'Voided trip'
+  --     END = '${payment_type}'
+  AND CAST(passenger_count AS STRING) = '${passenger_count}'
+  AND CAST(pickup_hour AS STRING) = '${pickup_hour}'
+  AND CASE pickup_dayofweek
+        WHEN 1 THEN 'Sun'
+        WHEN 2 THEN 'Mon'
+        WHEN 3 THEN 'Tue'
+        WHEN 4 THEN 'Wed'
+        WHEN 5 THEN 'Thu'
+        WHEN 6 THEN 'Fri'
+        WHEN 7 THEN 'Sat'
+      END = '${pickup_dayofweek}'
+  AND store_and_fwd_flag = '${store_and_fwd_flag}'
+GROUP BY payment_type
+ORDER BY payment_label;
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
